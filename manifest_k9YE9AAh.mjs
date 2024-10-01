5603f45dacb8d8e237341f5f7f3158ee4be397a6
Ignacio Cordoba <theignaciocordoba@gmail.com>
2024-10-01 14:34:20 +0000

    chore: move files to root

--- manifest_k9YE9AAh.mjs
+++ manifest_k9YE9AAh.mjs
@@ -1,31 +1,46 @@
-import '@astrojs/internal-helpers/path';
-import 'cookie';
-import 'kleur/colors';
-import 'string-width';
-import 'html-escaper';
-import 'clsx';
-import './chunks/astro_cqkYRnFf.mjs';
-import { compile } from 'path-to-regexp';
+import "@astrojs/internal-helpers/path";
+import "cookie";
+import "kleur/colors";
+import "string-width";
+import "html-escaper";
+import "clsx";
+import "./chunks/astro_cqkYRnFf.mjs";
+import { compile } from "path-to-regexp";
 
 if (typeof process !== "undefined") {
   let proc = process;
   if ("argv" in proc && Array.isArray(proc.argv)) {
-    if (proc.argv.includes("--verbose")) ; else if (proc.argv.includes("--silent")) ; else ;
+    if (proc.argv.includes("--verbose"));
+    else if (proc.argv.includes("--silent"));
+    else;
   }
 }
 
 function getRouteGenerator(segments, addTrailingSlash) {
-  const template = segments.map((segment) => {
-    return "/" + segment.map((part) => {
-      if (part.spread) {
-        return `:${part.content.slice(3)}(.*)?`;
-      } else if (part.dynamic) {
-        return `:${part.content}`;
-      } else {
-        return part.content.normalize().replace(/\?/g, "%3F").replace(/#/g, "%23").replace(/%5B/g, "[").replace(/%5D/g, "]").replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-      }
-    }).join("");
-  }).join("");
+  const template = segments
+    .map((segment) => {
+      return (
+        "/" +
+        segment
+          .map((part) => {
+            if (part.spread) {
+              return `:${part.content.slice(3)}(.*)?`;
+            } else if (part.dynamic) {
+              return `:${part.content}`;
+            } else {
+              return part.content
+                .normalize()
+                .replace(/\?/g, "%3F")
+                .replace(/#/g, "%23")
+                .replace(/%5B/g, "[")
+                .replace(/%5D/g, "]")
+                .replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+            }
+          })
+          .join("")
+      );
+    })
+    .join("");
   let trailing = "";
   if (addTrailingSlash === "always" && segments.length) {
     trailing = "/";
@@ -41,15 +56,20 @@ function deserializeRouteData(rawRouteData) {
     pattern: new RegExp(rawRouteData.pattern),
     params: rawRouteData.params,
     component: rawRouteData.component,
-    generate: getRouteGenerator(rawRouteData.segments, rawRouteData._meta.trailingSlash),
+    generate: getRouteGenerator(
+      rawRouteData.segments,
+      rawRouteData._meta.trailingSlash
+    ),
     pathname: rawRouteData.pathname || void 0,
     segments: rawRouteData.segments,
     prerender: rawRouteData.prerender,
     redirect: rawRouteData.redirect,
-    redirectRoute: rawRouteData.redirectRoute ? deserializeRouteData(rawRouteData.redirectRoute) : void 0,
+    redirectRoute: rawRouteData.redirectRoute
+      ? deserializeRouteData(rawRouteData.redirectRoute)
+      : void 0,
     fallbackRoutes: rawRouteData.fallbackRoutes.map((fallback) => {
       return deserializeRouteData(fallback);
-    })
+    }),
   };
 }
 
@@ -58,7 +78,7 @@ function deserializeManifest(serializedManifest) {
   for (const serializedRoute of serializedManifest.routes) {
     routes.push({
       ...serializedRoute,
-      routeData: deserializeRouteData(serializedRoute.routeData)
+      routeData: deserializeRouteData(serializedRoute.routeData),
     });
     const route = serializedRoute;
     route.routeData = deserializeRouteData(serializedRoute.routeData);
@@ -71,10 +91,75 @@ function deserializeManifest(serializedManifest) {
     assets,
     componentMetadata,
     clientDirectives,
-    routes
+    routes,
   };
 }
 
-const manifest = deserializeManifest({"adapterName":"","routes":[{"file":"","links":[],"scripts":[{"type":"external","value":"/_astro/hoisted.kPPQrZRG.js"}],"styles":[{"type":"external","src":"/_astro/index.KUocwji0.css"}],"routeData":{"route":"/","type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":false,"fallbackRoutes":[],"_meta":{"trailingSlash":"ignore"}}}],"site":"https://theignaciocordoba.com","base":"/","trailingSlash":"ignore","compressHTML":true,"componentMetadata":[["/home/nachovz/dev/astro/personal-website/src/pages/index.astro",{"propagation":"none","containsHead":true}]],"renderers":[],"clientDirectives":[["idle","(()=>{var i=t=>{let e=async()=>{await(await t())()};\"requestIdleCallback\"in window?window.requestIdleCallback(e):setTimeout(e,200)};(self.Astro||(self.Astro={})).idle=i;window.dispatchEvent(new Event(\"astro:idle\"));})();"],["load","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).load=e;window.dispatchEvent(new Event(\"astro:load\"));})();"],["media","(()=>{var s=(i,t)=>{let a=async()=>{await(await i())()};if(t.value){let e=matchMedia(t.value);e.matches?a():e.addEventListener(\"change\",a,{once:!0})}};(self.Astro||(self.Astro={})).media=s;window.dispatchEvent(new Event(\"astro:media\"));})();"],["only","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).only=e;window.dispatchEvent(new Event(\"astro:only\"));})();"],["visible","(()=>{var r=(i,c,s)=>{let n=async()=>{await(await i())()},t=new IntersectionObserver(e=>{for(let o of e)if(o.isIntersecting){t.disconnect(),n();break}});for(let e of s.children)t.observe(e)};(self.Astro||(self.Astro={})).visible=r;window.dispatchEvent(new Event(\"astro:visible\"));})();"]],"entryModules":{"\u0000@astro-page:src/pages/index@_@astro":"pages/index.astro.mjs","\u0000@astro-renderers":"renderers.mjs","\u0000empty-middleware":"_empty-middleware.mjs","/src/pages/index.astro":"chunks/pages/index_5uJ_idW2.mjs","\u0000@astrojs-manifest":"manifest_k9YE9AAh.mjs","/astro/hoisted.js?q=0":"_astro/hoisted.kPPQrZRG.js","astro:scripts/before-hydration.js":""},"assets":[]});
+const manifest = deserializeManifest({
+  adapterName: "",
+  routes: [
+    {
+      file: "",
+      links: [],
+      scripts: [{ type: "external", value: "/hoisted.kPPQrZRG.js" }],
+      styles: [{ type: "external", src: "/index.KUocwji0.css" }],
+      routeData: {
+        route: "/",
+        type: "page",
+        pattern: "^\\/$",
+        segments: [],
+        params: [],
+        component: "src/pages/index.astro",
+        pathname: "/",
+        prerender: false,
+        fallbackRoutes: [],
+        _meta: { trailingSlash: "ignore" },
+      },
+    },
+  ],
+  site: "https://theignaciocordoba.com",
+  base: "/",
+  trailingSlash: "ignore",
+  compressHTML: true,
+  componentMetadata: [
+    [
+      "/home/nachovz/dev/astro/personal-website/src/pages/index.astro",
+      { propagation: "none", containsHead: true },
+    ],
+  ],
+  renderers: [],
+  clientDirectives: [
+    [
+      "idle",
+      '(()=>{var i=t=>{let e=async()=>{await(await t())()};"requestIdleCallback"in window?window.requestIdleCallback(e):setTimeout(e,200)};(self.Astro||(self.Astro={})).idle=i;window.dispatchEvent(new Event("astro:idle"));})();',
+    ],
+    [
+      "load",
+      '(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).load=e;window.dispatchEvent(new Event("astro:load"));})();',
+    ],
+    [
+      "media",
+      '(()=>{var s=(i,t)=>{let a=async()=>{await(await i())()};if(t.value){let e=matchMedia(t.value);e.matches?a():e.addEventListener("change",a,{once:!0})}};(self.Astro||(self.Astro={})).media=s;window.dispatchEvent(new Event("astro:media"));})();',
+    ],
+    [
+      "only",
+      '(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).only=e;window.dispatchEvent(new Event("astro:only"));})();',
+    ],
+    [
+      "visible",
+      '(()=>{var r=(i,c,s)=>{let n=async()=>{await(await i())()},t=new IntersectionObserver(e=>{for(let o of e)if(o.isIntersecting){t.disconnect(),n();break}});for(let e of s.children)t.observe(e)};(self.Astro||(self.Astro={})).visible=r;window.dispatchEvent(new Event("astro:visible"));})();',
+    ],
+  ],
+  entryModules: {
+    "\u0000@astro-page:src/pages/index@_@astro": "pages/index.astro.mjs",
+    "\u0000@astro-renderers": "renderers.mjs",
+    "\u0000empty-middleware": "_empty-middleware.mjs",
+    "/src/pages/index.astro": "chunks/pages/index_5uJ_idW2.mjs",
+    "\u0000@astrojs-manifest": "manifest_k9YE9AAh.mjs",
+    "/astro/hoisted.js?q=0": "hoisted.kPPQrZRG.js",
+    "astro:scripts/before-hydration.js": "",
+  },
+  assets: [],
+});
 
 export { manifest };
